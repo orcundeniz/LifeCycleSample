f2bf6aea44c684d2e5b70bc97ea4dc534dcab36f
Orçun Deniz <orcun.deniz@teb.com.tr>
2019-04-25 14:33:31 +0300

    Initial Commit

---
+++ LifeCycleSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeCycleSample
+//
+//  Created by Orçun Deniz on 25.04.2019.
+//  Copyright © 2019 Orcun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
